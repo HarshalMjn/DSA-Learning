e8ec0d24eaea3742dce46a2a70b05a652e90f352
HarhslaMjn <mahajanh006@gmail.com>
2023-08-16 10:12:33 +0530

    type6 solve

--- Parttren/type6.cpp
+++ Parttren/type6.cpp
@@ -2,3 +2,20 @@
 //    * *
 //    * * *
 //    * * * *
+#include <iostream> 
+using namespace std;
+
+int main() {
+    cin>>n;
+    int i = 1;
+    while(i<=n) {
+        int j ;
+        while(j<=i){
+            cout<<"*"<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
